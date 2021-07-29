12aaa7400144c5f51c5a36106098cd88bf2466cb
sks <wsglko1@gmail.com>
2021-07-30 00:13:00 +0530

    home screen modified

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import React,{useEffect, useState} from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text, Title, Card, Subheading, Button } from 'react-native-paper';
-import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
+import moment from 'moment';
 
 const HomeScreen = ({ navigation }) => {
     const [user, setUser] = useState("");
     const [userRole, setUserRole] = useState("");
     useEffect(() => {
-        AsyncStorage.getItem("user_name").then((value) => setUser(value));
-        AsyncStorage.getItem("user_role").then((value) => setUserRole(value));
+        
     }, [])
     return (
         <View style={styles.container}>
